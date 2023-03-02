a1de481168d101e71dc74debf74314c710dbc744
lugalu <luizjaf01@gmail.com>
2023-03-02 20:51:46 -0300

    Feat: Initial Configuration

---
+++ HeathStoneExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeathStoneExample
+//
+//  Created by Lugalu on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
