c559b7aa5b055f34a787ca4ca933a5bd041309ce
lugalu <luizjaf01@gmail.com>
2023-03-06 20:08:36 -0300

    Feat: File for API key and endpoint this file will be added to gitignore to not leak credentials.

---
+++ HearthstoneExample/API Keys.swift
@@ -0,0 +1,15 @@
+//
+//  API Keys.swift
+//  HearthstoneExample
+//
+//  Created by Lugalu on 06/03/23.
+//
+
+import Foundation
+
+final class APIKEYS {
+    static let Rapid_API_Key: String = ""
+    static let Rapid_API_Endpoint = "omgvamp-hearthstone-v1.p.rapidapi.com";
+    
+    private init(){}
+}
